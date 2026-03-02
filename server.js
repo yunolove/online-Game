ebc674cbb0e1a0dbf7b39d7f59e470548de3c41e
yunolove <36820525+yunolove@users.noreply.github.com>
2026-03-02 17:47:52 +0900

    Update server.js

--- server.js
+++ server.js
@@ -2,23 +2,17 @@ const express = require("express");
 const http = require("http").createServer(express().use(express.static("."))); 
 const io = require("socket.io")(http);
 
-let players = {};
-
 io.on("connection", (socket) => {
-  players[socket.id] = { x: 250, y: 200, color: "blue" };
-  io.emit("update", players);
+  console.log("누군가 입장함!");
 
-  socket.on("move", (data) => {
-    if (players[socket.id]) {
-      players[socket.id].x += data.x;
-      players[socket.id].y += data.y;
-      io.emit("update", players);
-    }
+  // 누군가 메시지를 보내면
+  socket.on("chat message", (msg) => {
+    // 모든 사람에게 그 메시지를 다시 뿌림
+    io.emit("chat message", msg);
   });
 
   socket.on("disconnect", () => {
-    delete players[socket.id];
-    io.emit("update", players);
+    console.log("누군가 퇴장함");
   });
 });
 
