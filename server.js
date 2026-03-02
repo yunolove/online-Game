2d8c471f1ad1aa18fc6c780f2ce4cd5426b429cd
yunolove <36820525+yunolove@users.noreply.github.com>
2026-03-02 17:55:15 +0900

    Update server.js

--- server.js
+++ server.js
@@ -3,19 +3,15 @@ const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
-// 기본 페이지 접속 시 index.html을 보여줌
-app.use(express.static("."));
+// "/" 주소로 들어오면 무조건 chat_room.html을 보여줌 (index.html 무시)
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/chat_room.html");
+});
 
 io.on("connection", (socket) => {
-  console.log("새 유저 접속");
-
-  // 유저가 'message'라는 이름으로 글을 보내면
-  socket.on("message", (msg) => {
-    // 접속한 모든 사람에게 그 글을 그대로 뿌림
-    io.emit("message", msg);
+  socket.on("chat_msg", (msg) => {
+    io.emit("chat_msg", msg); // 모든 사람에게 전달
   });
 });
 
-http.listen(process.env.PORT || 3000, () => {
-  console.log("채팅 전용 서버 가동 중!");
-});
+http.listen(process.env.PORT || 3000);
